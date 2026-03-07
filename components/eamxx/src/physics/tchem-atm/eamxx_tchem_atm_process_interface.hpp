f164bc6f324c15c4999759a361ec963c54bb85ab
Oscar H. Diaz-Ibarra <odiazib@sandia.gov>
2026-03-06 16:08:51 -0800

    save

---
+++ components/eamxx/src/physics/tchem-atm/eamxx_tchem_atm_process_interface.hpp
@@ -0,0 +1,31 @@
+#ifndef EAMXX_TCHEM_ATM_PROCESS_INTERFACE_HPP
+#define EAMXX_TCHEM_ATM_PROCESS_INTERFACE_HPP
+#include <tchem-atm/TChem.hpp>
+#include <string>
+#include "share/atm_process/atmosphere_process.hpp"
+
+namespace scream {
+
+class TChemATM : public AtmosphereProcess {
+ public:
+  TChemATM(const ekat::Comm& comm, const ekat::ParameterList& params);
+
+  std::string name() const override { return "tchem_atm"; }
+  void create_requests() override;
+  AtmosphereProcessType type() const override {
+    return AtmosphereProcessType::Physics;
+  }
+
+ protected:
+  void initialize_impl(const RunType run_type) override;
+  void run_impl(const double dt) override;
+  void finalize_impl() override {}
+
+ private:
+  int get_len_temporary_views();
+  void init_temporary_views();
+};
+
+}  // namespace scream
+
+#endif  // EAMXX_TCHEM_ATM_PROCESS_INTERFACE_HPP
