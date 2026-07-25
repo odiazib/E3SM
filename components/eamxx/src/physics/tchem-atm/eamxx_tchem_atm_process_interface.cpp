49fcefe892b5032a7f4dc9c0c704bc383bec9209
Oscar H. Diaz-Ibarra <odiazib@sandia.gov>
2026-07-24 18:12:39 -0600

    get_out file at initialization.

--- components/eamxx/src/physics/tchem-atm/eamxx_tchem_atm_process_interface.cpp
+++ components/eamxx/src/physics/tchem-atm/eamxx_tchem_atm_process_interface.cpp
@@ -134,6 +134,23 @@ void TChemATM::initialize_impl(const RunType /* run_type */) {
   m_state_vec_dim      = TChem::Impl::getStateVectorSize(m_kmcd.nSpec);
   m_species_names_host = m_kmd.sNames_.view_host();
 
+  // Cache tracer field views to avoid per-timestep string map lookups.
+  constexpr int num_tracer_cnst = 3;
+  m_tracer_views.resize(m_n_active_vars);
+  for (int i = 0; i < m_n_active_vars; ++i) {
+    const std::string sname(&m_species_names_host(i, 0));
+    m_tracer_views[i] = get_field_out(sname).get_view<Real **>();
+  }
+  m_cnst_tracer_views.resize(num_tracer_cnst);
+  for (int j = 0; j < num_tracer_cnst; ++j) {
+    const std::string sname(&m_species_names_host(m_kmcd.M_index + 6 + j, 0));
+    m_cnst_tracer_views[j] = get_field_out(sname).get_view<Real **>();
+  }
+  if (m_o3_species_index >= 0) {
+    const std::string o3_name(&m_species_names_host(m_o3_species_index, 0));
+    m_o3_field_view = get_field_out(o3_name).get_view<Real **>();
+  }
+
   m_state = explicit_euler_type::real_type_2d_view_type("tchem_state", m_nbatch, m_state_vec_dim);
   const int m_photo_reactions = mam4::mo_photo::phtcnt;
   m_photo_rates = explicit_euler_type::real_type_2d_view_type("tchem_photo_rates", m_nbatch, m_photo_reactions);
@@ -460,11 +477,9 @@ void TChemATM::run_impl(const double dt) {
     const auto& cldfrac = get_field_in("cldfrac_tot").get_view<const Real **>();
 
     view_2d o3_field;
-    bool have_o3_field = false;
-    if (m_o3_species_index >= 0) {
-      const std::string o3_name(&m_species_names_host(m_o3_species_index, 0));
-      o3_field = get_field_out(o3_name).get_view<Real **>();
-      have_o3_field = true;
+    bool have_o3_field = (m_o3_species_index >= 0);
+    if (have_o3_field) {
+      o3_field = m_o3_field_view;
     }
 
     // ozone column buffer (preallocated in initialize_impl)
@@ -556,9 +571,7 @@ void TChemATM::run_impl(const double dt) {
 
   // std::cout << "[TChemATM] Done fill_state_column_from_field\n";
   for (int ivar = 0; ivar < m_n_active_vars; ++ivar) {
-    const auto& tracer_name = std::string(&m_species_names_host(ivar, 0));
-    // std::cout << "[TChemATM] Filling state column for tracer " << tracer_name << "\n";
-    const auto& q_tracer = get_field_out(tracer_name).get_view< Real **>();
+    const auto& q_tracer = m_tracer_views[ivar];
     // Use sampling-aware pack to only pack selected samples (m_sample_icol/ilev)
     tchem::pack_wet_mmr_into_state(state, q_tracer, qv, m_sample_icol, m_sample_ilev,
                 m_nsamples, ivar + 3, m_species_mw[ivar],
@@ -597,11 +610,8 @@ void TChemATM::run_impl(const double dt) {
 
 
   for (int j = 0; j < num_tracer_cnst; ++j) {
-    const auto& tracer_name = std::string(&m_species_names_host(m_kmcd.M_index + 6 + j, 0));
-    // std::cout << "[TChemATM] Filling state column for invariant tracer " << tracer_name << "\n";
-    const auto& q_tracer = get_field_out(tracer_name).get_view<Real **>();
     const int state_col_j = m_state_col + 6 + j;
-    tchem::pack_into_state(state, q_tracer, m_sample_icol, m_sample_ilev,
+    tchem::pack_into_state(state, m_cnst_tracer_views[j], m_sample_icol, m_sample_ilev,
                            m_nsamples, state_col_j,
                            "tchem_compute_cnst_tracer");
   }
@@ -644,11 +654,9 @@ void TChemATM::run_impl(const double dt) {
 #endif
   // After the TChem run, convert dry-vmr state back to wet-mmr tracer fields.
   for (int ivar = 0; ivar < m_n_active_vars; ++ivar) {
-    const auto& tracer_name = std::string(&m_species_names_host(ivar, 0));
-    const auto& q_tracer = get_field_out(tracer_name).get_view< Real **>();
     // Unpack only sampled entries from TChem state back into wet-mmr tracer field
-    tchem::unpack_wet_mmr_from_state(q_tracer, state, qv, m_sample_icol, m_sample_ilev,
-                  m_nsamples, ivar + 3, m_species_mw[ivar],
+    tchem::unpack_wet_mmr_from_state(m_tracer_views[ivar], state, qv, m_sample_icol,
+                  m_sample_ilev, m_nsamples, ivar + 3, m_species_mw[ivar],
                   "tchem_copy_back_state_tracer");
   } 
 
