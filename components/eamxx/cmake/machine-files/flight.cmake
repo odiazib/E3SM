ffa75f474a98d11eafe72a451f0363bd5c250853
Oscar H. Diaz-Ibarra <odiazib@sandia.gov>
2026-09-18 14:40:21 -0600

    using openmp for tests.

--- components/eamxx/cmake/machine-files/flight.cmake
+++ components/eamxx/cmake/machine-files/flight.cmake
@@ -1,20 +1,29 @@
 include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
 common_setup()
 
-# Flight is a Sandia SNL cluster with Intel compilers and OpenMPI
+set(EKAT_MACH_FILES_PATH ${CMAKE_CURRENT_LIST_DIR}/../../../../externals/ekat/cmake/machine-files)
 
 # Input data location
 set(SCREAM_INPUT_ROOT "/projects/ccsm/inputdata" CACHE STRING "")
 
-# Intel compiler-specific flags
-if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
-  # Disable some Intel compiler remarks that are verbose
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -diag-disable=remark" CACHE STRING "" FORCE)
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -diag-disable=remark" CACHE STRING "" FORCE)
+# NetCDF and PnetCDF paths
+# These are set via environment variables from modules, but we need to pass them to CMake
+if (DEFINED ENV{NETCDF_C_PATH})
+  set(NetCDF_C_PATH "$ENV{NETCDF_C_PATH}" CACHE STRING "")
+endif()
+set(NetCDF_Fortran_PATH "/projects/sems/install/boca/acme/manual/netcdf-fortran/4.6.3" CACHE STRING "")
+if (DEFINED ENV{PNETCDF_PATH})
+  set(PnetCDF_C_PATH "$ENV{PNETCDF_PATH}" CACHE STRING "")
+  set(PnetCDF_Fortran_PATH "$ENV{PNETCDF_PATH}" CACHE STRING "")
 endif()
 
-# Use Intel MKL for BLAS/LAPACK
-set(SCREAM_LINK_OPTIONS "-mkl" CACHE STRING "")
+# Get AMD arch settings
+include(${EKAT_MACH_FILES_PATH}/kokkos/intel-skx.cmake)
 
-# OpenMPI settings
-set(EKAT_MPI_NP_FLAG "-np" CACHE STRING "")
+# Add OpenMP settings in standalone mode OR e3sm with compile_threaded=ON
+if (NOT "${PROJECT_NAME}" STREQUAL "E3SM" OR compile_threaded)
+  include(${EKAT_MACH_FILES_PATH}/kokkos/openmp.cmake)
+endif()
+
+# Use srun for standalone testing
+include(${EKAT_MACH_FILES_PATH}/mpi/srun.cmake)
